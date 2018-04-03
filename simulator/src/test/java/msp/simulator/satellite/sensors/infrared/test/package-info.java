d3790be5917e26658e27aff99a9aaa4ed6df3643
braedenborg <36320083+braedenborg@users.noreply.github.com>
2018-04-03 17:05:35 +1000

    Update package-info.java

--- simulator/src/test/java/msp/simulator/satellite/sensors/infrared/test/package-info.java
+++ simulator/src/test/java/msp/simulator/satellite/sensors/infrared/test/package-info.java
@@ -1,7 +1,7 @@
 /* Copyright 2017-2018 Melbourne Space Program */
 
 /**
- * Unit tests for the satellite sensors modules.
- * @author Florian CHAUBEYRE
+ * Unit tests for the satellite infrared sensors.
+ * @author Braeden BORG
  */
-package msp.simulator.satellite.sensors.test;
+package msp.simulator.satellite.sensors.infrared.test;
