b69ef20584048e61543dcbc85da869d8d04a99c7
braedenborg <36320083+braedenborg@users.noreply.github.com>
2018-04-05 14:30:30 +1000

    Delete package-info.java

--- simulator/src/main/java/msp/simulator/satellite/sensors/infrared/package-info.java
+++
@@ -1,9 +0,0 @@
-/* Copyright 2017-2018 Melbourne Space Program */
-
-/**
- * This package sets up the infrared sensors and their functionality in
- * determining Nadir for the satellite
- * 
- * @author Braeden BORG
- */
-package msp.simulator.satellite.sensors.infrared;
